42bfc72eaa8b9f16a9c3f0e31979b01649072091
WojciechKaluzinski <profesorprzecinek@wp.pl>
2019-12-02 14:01:06 +0100

    zmiana ip znów

--- src/main/java/MainKlient.java
+++ src/main/java/MainKlient.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class MainKlient {
     public static void main(String[] args) throws Exception {
 
-        Klient klient = new Klient("192.168.56.1", 19);
+        Klient klient = new Klient("192.168.8.119", 19);
         klient.ramka.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         klient.ramka.setVisible(true);
         klient.play();
