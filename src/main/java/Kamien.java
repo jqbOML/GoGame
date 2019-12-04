e518894886b609e2732c4802c0a771f7cfbae0fd
WojciechKaluzinski <profesorprzecinek@wp.pl>
2019-12-04 12:13:32 +0100

    dodany test metody czyKolega

--- src/main/java/Kamien.java
+++ src/main/java/Kamien.java
@@ -9,15 +9,17 @@ public class Kamien {
         this.kolor = nowy_kolor;
     };
 
-
+//czyOddech sprawdza czy kamień ma w swoim zasięgu puste pole (nie bierze pod uwagę kolegi)
     public  boolean czyOddech(int x, int y, Kamien[][] kamien){
-        return kamien[x + 1][y].kolor == 0 || kamien[x - 1][y].kolor == 0 || kamien[x][y + 1].kolor == 0 || kamien[x][y - 1].kolor == 0
-                || kamien[x + 1][y].kolor == kolor || kamien[x - 1][y].kolor == kolor || kamien[x][y + 1].kolor == kolor || kamien[x][y - 1].kolor == kolor;
+        return kamien[x + 1][y].kolor == 0 || kamien[x - 1][y].kolor == 0 || kamien[x][y + 1].kolor == 0 || kamien[x][y - 1].kolor == 0;
+                //|| kamien[x + 1][y].kolor == kolor || kamien[x - 1][y].kolor == kolor || kamien[x][y + 1].kolor == kolor || kamien[x][y - 1].kolor == kolor;
     }
 
+    List<Kamien> lista = new ArrayList<>();
+
     public ArrayList<Kamien> czyKolega (int a, int b, Kamien[][] kamien){
-        List<Kamien> lista = new ArrayList<>();
-        if (!lista.contains(kamien[a + 1][b]) && kamien[a + 1][b].kolor==kolor){
+
+        if (lista.contains(kamien[a + 1][b])==false && kamien[a + 1][b].kolor==kolor){
 
           if (czyOddech(a+1, b, kamien)){
               lista.clear();
@@ -26,7 +28,7 @@ public class Kamien {
           lista.add(kamien[a+1][b]);
           czyKolega(a+1, b, kamien);
     }
-        if (!lista.contains(kamien[a - 1][b]) && kamien[a - 1][b].kolor == kolor){
+        if (lista.contains(kamien[a - 1][b]) == false && kamien[a - 1][b].kolor == kolor){
 
             if (czyOddech(a-1,b,kamien)){
                 lista.clear();
@@ -35,7 +37,7 @@ public class Kamien {
             lista.add(kamien[a-1][b]);
             czyKolega(a-1, b,kamien);
         }
-        if (!lista.contains(kamien[a][b+1]) && kamien[a][b+1].kolor==kolor){
+        if (lista.contains(kamien[a][b+1]) == false && kamien[a][b+1].kolor==kolor){
 
             if (czyOddech(a,b+1,kamien)){
                 lista.clear();
@@ -44,7 +46,7 @@ public class Kamien {
             lista.add(kamien[a][b+1]);
             czyKolega(a, b+1,kamien);
         }
-        if (!lista.contains(kamien[a][b-1]) && kamien[a][b-1].kolor==kolor){
+        if (lista.contains(kamien[a][b-1]) == false && kamien[a][b-1].kolor==kolor){
 
             if (czyOddech(a,b-1,kamien)){
                 lista.clear();
