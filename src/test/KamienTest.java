73f699b6cd05cdf0c6bf42203fbceab6bb07013a
WojciechKaluzinski <profesorprzecinek@wp.pl>
2019-12-04 12:30:38 +0100

    dodany test metody czyKolega poprawiona

--- src/test/KamienTest.java
+++ src/test/KamienTest.java
@@ -26,17 +26,12 @@ class KamienTest {
     @Test
     void czyKolegatest() {
 
-        List<Kamien> testlista = new ArrayList<>();
+
         kamienie[3][2] = new Kamien(1);
-        testlista.add(kamienie[3][2]);
         kamienie[3][1] = new Kamien(1);
-        testlista.add(kamienie[3][1]);
         kamienie[3][3] = new Kamien(1);
-        testlista.add(kamienie[3][3]);
         kamienie[2][2] = new Kamien(1);
-        testlista.add(kamienie[2][2]);
-        kamienie[4][2] = new Kamien(1);
-        testlista.add(kamienie[4][2]);//kamienie otoczone, dodane do listy testowej
+        kamienie[4][2] = new Kamien(1);//5 kamieni do usunięcia
 
         kamienie[3][0] = new Kamien(2);
         kamienie[4][1] = new Kamien(2);
@@ -45,9 +40,9 @@ class KamienTest {
         kamienie[3][4] = new Kamien(2);
         kamienie[2][3] = new Kamien(2);
         kamienie[1][2] = new Kamien(2);
-        kamienie[2][1] = new Kamien(2); //kmienie otaczające*/
+        kamienie[2][1] = new Kamien(2); //kmienie otaczające
 
-        assertEquals(testlista.size(), kamienie[3][2].czyKolega(3, 2, kamienie).size());
+        assertEquals(5, kamienie[3][2].czyKolega(3, 2, kamienie).size());
 
     }
 
