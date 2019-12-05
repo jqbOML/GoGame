61c12490f17c6cd2293cbdc679ea94202d197c2e
tk27 <thekibi27@gmail.com>
2019-12-05 11:13:49 +0100

    Dodanie uml

--- src/main/java/MainKlient.java
+++ src/main/java/MainKlient.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class MainKlient {
     public static void main(String[] args) throws Exception {
 
-        Klient klient = new Klient("127.0.0.1", 19);
+        Klient klient = new Klient("10.182.237.244", 19);
         klient.ramka.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         klient.ramka.setVisible(true);
         klient.graj();
